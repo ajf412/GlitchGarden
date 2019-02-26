4bd2224609b83d7c37e263ac8765a42023d62ab5
ajf412 <aaronjfaulkner@yahoo.com>
2019-02-25 20:19:16 -0500

    Big update.  Spawners for enemies now spawn zombie farmers.  Zombie farmers now walk downfield.  Added player coin chests.  Animated coin chests.  Added defender Archer_01, animated Archer_01.  Added arrow for archer.  More work for arrow is needed.

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] GameObject[] mobs;
+    [SerializeField] float minSpawnDelay = 1.0f;
+    [SerializeField] float maxSpawnDelay = 5.0f;
+
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(mobs[0], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, releasePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack(float damage)
+    {
+        if (projectile && releasePoint)
+        {
+            print("Shooting");
+            Instantiate(projectile, releasePoint.transform.position, Quaternion.identity);
+        }
+    }
+}
